eb21140cbb1f635c825b7a7e164402f4eb73d06f
priyas9595 <59990900+priyas9595@users.noreply.github.com>
2020-10-13 17:41:50 +0530

    log files changes

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,9 +2,14 @@ const WebSocket = require("ws");
 
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
-connection.onopen =()=>
-{ connection.send("Hello from the client!");
+const filePath =document.getElementById('logFilePath').value;
+connection.onopen = () => {
+    if(filePath)
+    {
+    connection.send(filePath);
+    }
 }
 connection.onmessage = (event) => {
-    logWindow.innerHTML = event.data;
+    const logs = event.data.split("\n").join("<hr>")
+  logWindow.innerHTML = logs
 };
