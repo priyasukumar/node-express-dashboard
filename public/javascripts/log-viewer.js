a9993d4eaf3aacbfab4ce0380e97216ccb15550e
priyas9595 <59990900+priyas9595@users.noreply.github.com>
2020-10-13 16:46:03 +0530

    ws event handler changes

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,8 +1,10 @@
 const WebSocket = require("ws");
 
 const connection = new WebSocket("ws://localhost:3000");
-const logwindow = document.querySelector("#log-window");
-connection.onopen = connection.send("Hello from the client!");
+const logWindow = document.querySelector("#log-window");
+connection.onopen =()=>
+{ connection.send("Hello from the client!");
+}
 connection.onmessage = (event) => {
-    logwindow = event.data;
+    logWindow.innerHTML = event.data;
 };
